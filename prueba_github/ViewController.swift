e15566972dfa5fd10645aa8e698efe1298179e4b
Miguel angel Bogarin frias <Miguel@macbook-pro-de-miguel.local>
2015-12-20 22:31:46 -0700

    Initial Commit

---
+++ prueba_github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba_github
+//
+//  Created by Miguel angel Bogarin frias on 20/12/15.
+//  Copyright © 2015 Miguel angel Bogarin frias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
